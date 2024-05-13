c45f8286c800912d5d9d7a2edc7299275b4cd049
Chris Fleming <me@chrisfleming.org>
2024-05-13 17:59:12 +0100

    Don't autoformat python.

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,21 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Autoformat setting
+--
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat({ "cpp" }, true)
+set_autoformat({ "python" }, false)
+set_autoformat({ "fish" }, false)
+set_autoformat({ "lua" }, false)
+set_autoformat({ "perl" }, false)
+set_autoformat({ "yaml" }, false)
